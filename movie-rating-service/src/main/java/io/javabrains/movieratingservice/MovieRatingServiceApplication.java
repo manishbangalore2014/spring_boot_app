096ce299d95ab6ba8ab699deafd67992185c5253
MRajput2 <MRajput2@HIBACL67080.ad.harman.com>
2019-12-23 15:32:27 +0530

    f commit

---
+++ movie-rating-service/src/main/java/io/javabrains/movieratingservice/MovieRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.movieratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingServiceApplication.class, args);
+	}
+
+}
