2ecdecf02f6956a75a5c654f1d787342cc428108
MRajput2 <MRajput2@HIBACL67080.ad.harman.com>
2019-12-23 15:30:28 +0530

    first commit

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.moviecatalogservice.model.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		List<CatalogItem> list =Collections.singletonList(new CatalogItem("Transformar", "test", 5));
+		return list;
+	}
+
+}
